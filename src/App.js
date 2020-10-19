a3a215dec3656a9c003878a6ef0759c1326ad1f7
Boogle Staff <melvintseng@BoogleStaff.local>
2020-10-19 11:55:36 +0800

    React Table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState,useMemo } from 'react';
 import './App.css'
-import { makeStyles} from '@material-ui/core';
-import { useTable } from 'react-table'
+import Table from './ReactTableEx2/Table'
 
-function App() {
-  return (
-    <div>
+
+const App = () => {
+
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const doFetch = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      const body = await response.json()
+      const contacts = body.results
+      console.log(contacts)
+      setData(contacts)
+    }
+    doFetch()
+  }, [])
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "name",
+        accessor: "name",
+      },
+      {
+        Header: "URL",
+        accessor: "url",
+      },
       
-    </div>
+    ],
+    []
   )
+
+
+  return <div><Table columns={columns} data={data}/></div>
 }
 
 export default App
